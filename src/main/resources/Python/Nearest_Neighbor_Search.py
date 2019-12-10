d430b8a4080a754b7e3881b4fdbc7efbf9a2589e
luca <luca.digrazia94@gmail.com>
2019-12-10 16:10:40 +0100

    Creation of the Python stage.

--- src/main/resources/Python/Nearest_Neighbor_Search.py
+++ src/main/resources/Python/Nearest_Neighbor_Search.py
@@ -1,3 +1,17 @@
-f = open("./src/main/resources/Python/demofile2.txt", "a")
-f.write("1")
-f.close()
+import csv
+
+
+#Reading csv feature vectors files
+with open('./src/main/resources/Features_Vectors/changes_feature_vectors.csv','rt') as f:
+  changes_feature_vectors = csv.reader(f)
+#  for row in changes_feature_vectors:
+#        print(row)
+
+with open('./src/main/resources/Features_Vectors/query_feature_vectors.csv','rt') as f:
+  query_feature_vectors = csv.reader(f)
+
+import faiss                   # make faiss available
+index = faiss.IndexFlatL2(len(changes_feature_vectors[0]))   # build the index
+print(index.is_trained)
+index.add(changes_feature_vectors)                  # add vectors to the index
+print(index.ntotal)
