f231eda05bb10f6b297aa1dde8d59a130ef54e52
luca <luca.digrazia94@gmail.com>
2020-07-24 17:12:17 +0200

    Added HTML client.

---
+++ src/main/resources/WebInterface/main.js
@@ -0,0 +1,21 @@
+
+function search_function() {
+    // Selecting the input element and get its value
+    var inputVal = document.getElementById("query_old").value;
+    var inputVal2 = document.getElementById("query_new").value;
+
+
+    document.getElementById("myParagraph0").innerHTML ="<pre><font size=\"16\">Ten of the total code changes found are:\n\n</font></pre>";
+    document.getElementById("myParagraph1").innerHTML = "<pre><font size=\"14\"><span style='color:#9c0101'>-" + inputVal + "</span>\n<span style='color:#008000'>+" + inputVal2 + "</span>\n\n"+ "</font></pre>";
+    document.getElementById("myParagraph2").innerHTML ="<pre><font size=\"14\"><span style='color:#9c0101'>-" + inputVal + "</span>\n<span style='color:#008000'>+" + inputVal2 + "</span>\n\n"+ "</font></pre>";
+    document.getElementById("myParagraph3").innerHTML ="<pre><font size=\"14\"><span style='color:#9c0101'>-" + inputVal + "</span>\n<span style='color:#008000'>+" + inputVal2 + "</span>\n\n"+ "</font></pre>";
+    document.getElementById("myParagraph4").innerHTML ="<pre><font size=\"14\"><span style='color:#9c0101'>-" + inputVal + "</span>\n<span style='color:#008000'>+" + inputVal2 + "</span>\n\n"+ "</font></pre>";
+    document.getElementById("myParagraph5").innerHTML ="<pre><font size=\"14\"><span style='color:#9c0101'>-" + inputVal + "</span>\n<span style='color:#008000'>+" + inputVal2 + "</span>\n\n"+ "</font></pre>";
+    document.getElementById("myParagraph6").innerHTML = "<pre><font size=\"14\"><span style='color:#9c0101'>-" + inputVal + "</span>\n<span style='color:#008000'>+" + inputVal2 + "</span>\n\n"+ "</font></pre>";
+    document.getElementById("myParagraph7").innerHTML ="<pre><font size=\"14\"><span style='color:#9c0101'>-" + inputVal + "</span>\n<span style='color:#008000'>+" + inputVal2 + "</span>\n\n"+ "</font></pre>";
+    document.getElementById("myParagraph8").innerHTML ="<pre><font size=\"14\"><span style='color:#9c0101'>-" + inputVal + "</span>\n<span style='color:#008000'>+" + inputVal2 + "</span>\n\n"+ "</font></pre>";
+    document.getElementById("myParagraph9").innerHTML ="<pre><font size=\"14\"><span style='color:#9c0101'>-" + inputVal + "</span>\n<span style='color:#008000'>+" + inputVal2 + "</span>\n\n"+ "</font></pre>";
+    document.getElementById("myParagraph10").innerHTML ="<pre><font size=\"14\"><span style='color:#9c0101'>-" + inputVal + "</span>\n<span style='color:#008000'>+" + inputVal2 + "</span>\n\n"+ "</font></pre>";
+
+}
+
