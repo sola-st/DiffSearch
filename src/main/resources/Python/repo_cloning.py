ff317fc0c2b0dc9e1cbe576edfdec1f838c5abae
luca <luca.digrazia94@gmail.com>
2021-02-05 11:03:26 +0100

    Repository list added.

--- src/main/resources/Python/repo_cloning.py
+++ src/main/resources/Python/repo_cloning.py
@@ -4,29 +4,33 @@ import re
 
 import pygit2 as git
 
+filenameInput = "../Input/topJavaMavenProjects.txt"
+pathOutput = "../GitHub_Java/"
 
-def repo_cloning(filenameInput: str, pathOutput: str) -> None:
-    with open(filenameInput) as fh:
-        articles = json.load(fh)
+#with open(filenameInput) as fh:
+ #   articles = json.load(fh)
 
-    article_urls = [article['html_url'] for article in articles]
+with open(filenameInput) as f:
+    article_urls =  [line.rstrip() for line in f]
 
-    i = 0
-    for link in article_urls:
-        i += 1
+#article_urls = [article['html_url'] for article in articles]
+#article_urls = articles.values()
+i = 0
+for link in article_urls:
+    i += 1
 
-        # out = link.rsplit('/', 1)[-1].replace('.git', '')
-        out = re.sub('https://github.com/', '', link).replace('/', '-')
+    # out = link.rsplit('/', 1)[-1].replace('.git', '')
+    out = re.sub('https://github.com/', '', link).replace('/', '-')
 
-        if os.path.isdir(pathOutput + '/' + out):
-            print(str(i) + ') Already cloned', link)
+    if os.path.isdir(pathOutput + '/' + out):
+        print(str(i) + ') Already cloned', link)
 
+        continue
+
+    else:
+        print(str(i) + ') Cloning ' + link)
+        try:
+            git.clone_repository(link, pathOutput + '/' + out)
+        except Exception as e:
+            print('[Error] cloning repository:', str(e))
             continue
-
-        else:
-            print(str(i) + ') Cloning ' + link)
-            try:
-                git.clone_repository(link, pathOutput + '/' + out)
-            except Exception as e:
-                print('[Error] cloning repository:', str(e))
-                continue
