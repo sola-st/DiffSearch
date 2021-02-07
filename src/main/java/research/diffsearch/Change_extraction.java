f0d12de19118ba8c6dfefad21d5d568d0d5ccb7f
luca <luca.digrazia94@gmail.com>
2021-02-07 18:03:32 +0100

    Minor fix for extraction.

--- src/main/java/research/diffsearch/Change_extraction.java
+++ src/main/java/research/diffsearch/Change_extraction.java
@@ -757,7 +757,8 @@ public class Change_extraction {
             Scanner scanner = null;
             BufferedReader br = null;
             try {
-                scanner = new Scanner(new FileInputStream(f), "UTF-8");
+                //scanner = new Scanner(new FileInputStream(f), "UTF-8");
+                scanner = new Scanner(f, "UTF-8");
                 //br = new BufferedReader(new FileReader(f));
             } catch (IOException e) {
                 e.printStackTrace();
@@ -777,7 +778,7 @@ public class Change_extraction {
                 while (scanner.hasNextLine()) {
                // while ((line = br.readLine()) != null) {
                    // line += "  ";
-                   // System.out.print(line_num++ + "\n");
+                    //System.out.print(line_num++ + "\n");
                     line = scanner.nextLine() + "  ";
                     //if(line.contains("submittedNode.get"))
                       //  System.out.println("okay");
