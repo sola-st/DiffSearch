26ed7a54e7e53497c95abe51442db796cb1e0ca2
luca <luca.digrazia94@gmail.com>
2021-02-09 09:00:40 +0100

    Minor fix for the dataset creation script.

--- src/main/resources/Python/effectiveness_dataset_creation.py
+++ src/main/resources/Python/effectiveness_dataset_creation.py
@@ -11,14 +11,18 @@ datasetInput = "./sstubs5.json"
 with open(datasetInput) as fh:
    dataset = json.load(fh)
 
+total = len(dataset)
+counter = 0
 for code_change in dataset:
+    counter +=1
+    print("Commits performed", counter, "/", total)
     try:
         commit = code_change['fixCommitSHA1']
         parent_commit = code_change['fixCommitParentSHA1']
         projectName = code_change['projectName']
         command = "git --git-dir ../GitHub_Java/" + projectName + "/.git diff " + commit + " " + parent_commit + " >> ../java_patch/dataset.patch"
         os.system(command)
-        print(command)
+        #print(command)
     except:
         print('[Error]')
         continue
