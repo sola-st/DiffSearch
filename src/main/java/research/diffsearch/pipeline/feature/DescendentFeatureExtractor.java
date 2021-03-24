159260d61f86b95736ef3b6f94fd6b405e16d1dc
PaulBredl <st156256@stud.uni-stuttgart.de>
2021-03-24 11:40:22 +0100

    better memory usage for DescendentFeatureExtractor

--- src/main/java/research/diffsearch/pipeline/feature/DescendentFeatureExtractor.java
+++ src/main/java/research/diffsearch/pipeline/feature/DescendentFeatureExtractor.java
@@ -6,9 +6,6 @@ import research.diffsearch.Config;
 import research.diffsearch.pipeline.feature.FeatureVector.Section;
 import research.diffsearch.util.ProgrammingLanguage;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import static research.diffsearch.util.Util.isQueryKeyword;
 
 public class DescendentFeatureExtractor extends AbstractRecursiveFeatureExtractor {
@@ -19,29 +16,31 @@ public class DescendentFeatureExtractor extends AbstractRecursiveFeatureExtracto
 
     @Override
     public void extractFeaturesRecursive(Tree t, Section section, boolean isQuery) {
-        extractFeaturesRecursive(t, section, isQuery, new ArrayList<>());
-    }
-
-    private void extractFeaturesRecursive(Tree t, Section section, boolean isQuery, List<String> parentNodes) {
         var ruleNames = getProgrammingLanguage().getRuleNames();
-        String currentNodeText = Trees.getNodeText(t, ruleNames);
+        String parentChildText = Trees.getNodeText(t, ruleNames);
 
-        for (int i = 1; i < parentNodes.size(); i++) {
-            var parentNodeText = parentNodes.get(i);
-            if (shouldExtractFeature(parentNodeText, isQuery)) {
-                var feature = currentNodeText + ' ' + parentNodeText;
-
-                section.addFeature(feature);
+        if (!parentChildText.equals("querySnippet")) {
+            for (int i = 0; i < t.getChildCount(); i++) {
+                extractFeaturesForParent(t.getChild(i), section, isQuery, parentChildText);
             }
         }
 
-        List<String> newParentNodes = parentNodes;
-        if (!currentNodeText.equals("querySnippet")) {
-            newParentNodes = new ArrayList<>(parentNodes);
-            newParentNodes.add(currentNodeText);
-        }
         for (int i = 0; i < t.getChildCount(); i++) {
-            extractFeaturesRecursive(t.getChild(i), section, isQuery, newParentNodes);
+            extractFeaturesRecursive(t.getChild(i), section, isQuery);
+        }
+    }
+
+    private void extractFeaturesForParent(Tree t, Section section, boolean isQuery, String parentNode) {
+        var ruleNames = getProgrammingLanguage().getRuleNames();
+        var childNodeText = Trees.getNodeText(t, ruleNames);
+        if (shouldExtractFeature(childNodeText, isQuery)) {
+            var feature = parentNode + ' ' + childNodeText;
+
+            section.addFeature(feature);
+        }
+
+        for (int i = 0; i < t.getChildCount(); i++) {
+            extractFeaturesForParent(t.getChild(i), section, isQuery, parentNode);
         }
     }
 
