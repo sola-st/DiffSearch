b2e013d3e7e2e85d928a5ec9c1864b3333a52399
PaulBredl <st156256@stud.uni-stuttgart.de>
2021-05-05 13:36:36 +0200

    better naming.

--- src/main/java/research/diffsearch/main/RemoveEqualCodeChanges.java
+++ src/main/java/research/diffsearch/main/RemoveEqualCodeChanges.java
@@ -9,16 +9,16 @@ import static research.diffsearch.util.FilePathUtils.*;
 import static research.diffsearch.util.ProgrammingLanguage.JAVA;
 
 /**
+ * Removes code changes of the form a --> a.
  * @author Paul Bredl
- * @author Luca Di Grazia
  */
-public class Deduplication {
+public class RemoveEqualCodeChanges {
 
     public static void main(String[] args) throws IOException {
         var codeChanges = getCodeChanges(JAVA);
         Pipeline
                 .getFilter(MatchingPipeline::isNotEqualCodeChange)
-                .connect(getStringFileWriterPipeline(getChangesFilePath(JAVA) + ".deduplicated"))
+                .connect(getStringFileWriterPipeline(getChangesFilePath(JAVA) + ".copy"))
                 .execute(codeChanges);
     }
 }
--- src/main/resources/Recall/run_recall_script.sh
+++ src/main/resources/Recall/run_recall_script.sh
@@ -1,2 +1,2 @@
-mvn exec:java -Dexec.mainClass=research.diffsearch.main.App -Dexec.args="-fe -r -k 1000 -silent -lang java -vl 300 -extractors node;triangle;rulecount:1400 -mt 10" -e
-mvn exec:java -Dexec.mainClass=research.diffsearch.main.App -Dexec.args="-b -r -k 1000 -silent -lang java -vl 300 -extractors node;triangle;rulecount:1400 -mt 10" -e
+mvn exec:java -Dexec.mainClass=research.diffsearch.main.App -Dexec.args="-fe -r -k 5000 -silent -lang java -vl 300 -extractors node;triangle;rulecount:1400 -mt 10" -e
+mvn exec:java -Dexec.mainClass=research.diffsearch.main.App -Dexec.args="-b -r -k 5000 -silent -lang java -vl 300 -extractors node;triangle;rulecount:1400 -mt 10" -e
