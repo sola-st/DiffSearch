743d2c508280b11f4a84a89c8522e56312c18ba3
emymaria <68675647+emymaria@users.noreply.github.com>
2022-01-19 17:38:42 +0100

    Update FAISS_indexing_python.py

--- src/main/resources/Python/FAISS_indexing_python.py
+++ src/main/resources/Python/FAISS_indexing_python.py
@@ -34,13 +34,16 @@ def indexing(feature_in, index_out, dimension, nlist, tfidf=False):
     logger.info("Starting indexing")
     # quantiser = faiss.IndexFlatL2(dimension)
     # metric = faiss.METRIC_L2
+    nlist = 1
+    M = 8
+    nbits = 8
 
     if tfidf:
         # quantiser = faiss.IndexFlatIP(dimension)
         metric = faiss.METRIC_INNER_PRODUCT
 
-    index = faiss.IndexScalarQuantizer(dimension,faiss.ScalarQuantizer.QT_8bit)
-    # index = faiss.IndexLSH(dimension,8)
+    index = faiss.IndexPQ(dimension, M, nbits)
+    # index = faiss.IndexPQ(d,M,nbits)
 
     np_array = np.ascontiguousarray(changes_feature_vectors)
 
