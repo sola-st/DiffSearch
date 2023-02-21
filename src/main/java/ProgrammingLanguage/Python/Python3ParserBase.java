1cc33ff3888b60f8ad086f6da6d4c6ff6d016de2
augenfrosch <augenfrosch@okonnek.name>
2023-02-21 17:26:54 +0100

    Don't add newlines to the parse tree
    This fixes multiple issues when attempting to match newlines.

--- src/main/java/ProgrammingLanguage/Python/Python3ParserBase.java
+++ src/main/java/ProgrammingLanguage/Python/Python3ParserBase.java
@@ -1,6 +1,9 @@
 package ProgrammingLanguage.Python;
 
 import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.tree.ErrorNode;
+import org.antlr.v4.runtime.tree.ParseTreeListener;
+import org.antlr.v4.runtime.tree.TerminalNode;
 
 public abstract class Python3ParserBase extends Parser {
     protected Python3ParserBase(TokenStream input) {
@@ -14,4 +17,39 @@ public abstract class Python3ParserBase extends Parser {
     public boolean CannotBeDotLpEq() {
         return true;
     }
+    
+    /**
+     * Modified to not include {@code \n} nodes in parse tree;
+     * Original Documentation:
+     * 
+     * {@inheritDoc}
+     */
+	@Override
+	public Token consume() {
+		Token o = getCurrentToken();
+		if (o.getType() != EOF) {
+			getInputStream().consume();
+		}
+		boolean hasListener = _parseListeners != null && !_parseListeners.isEmpty();
+		if (_buildParseTrees || hasListener) {
+			if ( _errHandler.inErrorRecoveryMode(this) ) {
+				ErrorNode node = _ctx.addErrorNode(createErrorNode(_ctx,o));
+				if (_parseListeners != null) {
+					for (ParseTreeListener listener : _parseListeners) {
+						listener.visitErrorNode(node);
+					}
+				}
+			}
+			else if (!o.getText().equals("\n")) {
+				
+				TerminalNode node = _ctx.addChild(createTerminalNode(_ctx,o));
+				if (_parseListeners != null) {
+					for (ParseTreeListener listener : _parseListeners) {
+						listener.visitTerminal(node);
+					}
+				}
+			}
+		}
+		return o;
+	}
 }
